166a2cde6442e1be8529e1abed3fe21c94440ad4
Bang Zakir <zakir.christy@gmail.com>
2021-12-07 22:14:54 +0700

    update api

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'https://2cf0-125-162-213-215.ngrok.io/api/v1'
+    baseURL: 'https://5200-180-252-204-236.ngrok.io/api/v1'
 })
 
 export default Api
